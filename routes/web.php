55c7234466f728ce6e805c284a6ec20f35643802
Mario Tomas <tmario291@gmail.com>
2024-06-25 21:00:19 -0600

    change

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,11 @@ use App\Http\Controllers\Admin\ItemController;
 use App\Http\Controllers\ComparisonController;
 use App\Http\Controllers\Admin\ItemsComparisonsController;
 use App\Http\Controllers\Admin\VotesShowController;
-use Illuminate\Support\Facades\Auth;
 
-Auth::routes(['verify' => true]);
+Route::get('/', [ComparisonController::class, 'index'])->name('home');
+Route::post('/vote', [VoteController::class, 'store'])->middleware('auth')->name('vote.store');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', [ComparisonController::class, 'index'])->name('home');
-    Route::post('/vote', [VoteController::class, 'store'])->name('vote.store');
-});
-
-Route::middleware(['auth', 'admin', 'verified'])->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('admin', [HomeController::class, 'index'])->name('admin.index');
     Route::resource('items', ItemController::class)->names('admin.items');
     Route::resource('comparison', ItemsComparisonsController::class)->names('admin.comparisons');
